edc3402fbf88a0813a65d79270b4bb8cedbf24ba
Alexandre Julião <julas@MacBookAir.Home>
2026-05-16 17:45:46 +0100

    chore: add real Facebook Pixel ID

--- src/lib/fpixel.ts
+++ src/lib/fpixel.ts
@@ -1,4 +1,4 @@
-export const FB_PIXEL_ID = process.env.NEXT_PUBLIC_FACEBOOK_PIXEL_ID || "YOUR_PIXEL_ID";
+export const FB_PIXEL_ID = process.env.NEXT_PUBLIC_FACEBOOK_PIXEL_ID || "1589728065448469";
 
 export const pageview = () => {
   if (typeof window !== "undefined" && (window as any).fbq) {
